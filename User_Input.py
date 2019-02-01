d9572f2019248a501152d26da2023c056e882dac
Orthrin <gronzelg@gmail.com>
2019-02-01 03:34:54 +0300

    Adding first hello world program.

---
+++ User_Input.py
@@ -0,0 +1,7 @@
+#print("hello")
+#name = input("What is your name? ")
+#print("hello " + name)
+
+number = input("What is your favorite number? ") # get number from user
+triple = int(number) * 3
+print("your fav number is multiplied by three " + str(triple))
