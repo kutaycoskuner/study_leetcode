0ff45de1407d558aa2c08c712ed457272716eb66
Orthrin <gronzelg@gmail.com>
2019-02-01 04:11:47 +0300

    Adding user input and variable type examples

--- CookBook/User_Input.py
+++
@@ -1,7 +0,0 @@
-#print("hello")
-#name = input("What is your name? ")
-#print("hello " + name)
-
-number = input("What is your favorite number? ") # get number from user
-triple = int(number) * 3
-print("your fav number is multiplied by three " + str(triple))
